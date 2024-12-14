c19ea68a573b501b32ad0bb638bc3299f555265c
Klára Maxová <50149154+klaramax@users.noreply.github.com>
2024-12-14 14:22:29 +0100

    Fix select styling in create-gift.blade.php

--- Herd/pokus1/resources/views/create-gift.blade.php
+++ Herd/pokus1/resources/views/create-gift.blade.php
@@ -5,16 +5,12 @@
         <form action="{{ route('gift.store') }}" method="POST" class="flex flex-col items-center w-full gap-8">
             @csrf
             <x-input placeholder="Název dárku" type="text" name="name" required style="max-width:400px;margin-left:auto;margin-right:auto;" />
-            <div style="max-width:400px;margin-left:auto;margin-right:auto;">
-                <div style="max-width:400px;margin-left:auto;margin-right:auto;">
-                <select name="person_id" id="person_id" required class="w-full border p-2 rounded-md">
-                    <option value="">-- Pro koho --</option>
-                    @foreach ($persons as $person)
-                        <option value="{{ $person->id }}">{{ $person->name }}</option>
-                    @endforeach
-                </select>
-    </div>
-            </div>
+            <select name="person_id" id="person_id" required class="max-w-[400px] w-full border p-2 rounded-md">
+                <option value="">-- Pro koho --</option>
+                @foreach ($persons as $person)
+                    <option value="{{ $person->id }}">{{ $person->name }}</option>
+                @endforeach
+            </select>
             <x-input placeholder="Cena dárku" type="text" name="price" style="max-width:400px;margin-left:auto;margin-right:auto;" />
             <x-input placeholder="Odkaz na dárek" type="text" name="url" style="max-width:400px;margin-left:auto;margin-right:auto;" />
             <x-input placeholder="Název obchodu" type="text" name="where_bought" style="max-width:400px;margin-left:auto;margin-right:auto;" />
